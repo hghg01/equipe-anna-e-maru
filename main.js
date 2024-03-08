4fd63eca4f3ca24800f2b2879c4e90e4a4c28dfc
hghg01 <102838081+hghg01@users.noreply.github.com>
2024-03-08 11:45:31 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
- var nome_user = prompt('Qual é o seu nome?');
+ var nome_user = '';
  var fulano = document.querySelector('span');
+
+ while(nome_user =='') {
+    nome_user = prompt('Qual é o seu nome?');
+ }
 
  fulano.textContent = nome_user;
