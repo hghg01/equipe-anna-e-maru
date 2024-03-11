f9711617af9d91ecaec2638991604ce32447472f
hghg01 <102838081+hghg01@users.noreply.github.com>
2024-03-11 11:54:01 -0300

    .

--- jogo.js
+++ jogo.js
@@ -1,11 +1,15 @@
 let numeroSecreto = 7;
 
-let chute = prompt('Escolha um número de 0 a 10');
+let chute = '';
+
+while(chute == ''){
+    chute = prompt('Escolha um número de 0 a 10');
+}
 
 if(numeroSecreto == chute){
     alert('Parabéns, você acertou!!!');
 
 }else{
     alert('Tenta de novo, você errou');
-    
+
 }
